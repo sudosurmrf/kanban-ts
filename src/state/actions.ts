aee581b0f2afcf54096930f18176699f7ce996b9
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2025-10-17 23:59:02 -0700

    created an action state for all card movement and column movement.

---
+++ src/state/actions.ts
@@ -0,0 +1,12 @@
+import { ID } from "../types/ids";
+import { Label } from "../types/board";
+
+export type Action = 
+| { type: "ADD_CARD"; columnById: ID<"col">; title: string}
+| { type: "MOVE_CARD"; cardId: ID<"card">; from: ID<"col">; to: ID<"col">; index: number }
+| { type: "UPDATE_CARD"; cardId: ID<"card">; patch: Partial<{ title:string; description: string; label: Label; points: number }> }
+| { type: "ADD_COLUMN"; name: string }
+| { type: "RENAME_COLUMN"; columnById: ID<"col">; name: string }
+| { type: "DELETE_CARD"; cardId: ID<"card">; columnById: ID<"col">}
+| { type: "MOVE_COLUMN"; columnById: ID<"col">; fromIndex: number; toIndex: number };
+
